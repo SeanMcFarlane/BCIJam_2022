a48e9f33f9ed062cbc705b966214e1cb06c387d5
Sean McFarlane <seanmcfarlane115@gmail.com>
2022-12-03 12:33:16 -0800

    Set up main menu (WIP)

---
+++ BCIJam_2022/Assets/ExitGameScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGameScript : MonoBehaviour {
+	public void ExitApplication() {
+		Application.Quit();
+	}
+}
