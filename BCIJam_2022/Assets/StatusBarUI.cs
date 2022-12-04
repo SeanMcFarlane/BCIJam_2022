debd1080aa55271b9ff90a8f9d4941fc8cc60ad2
Sean McFarlane <seanmcfarlane115@gmail.com>
2022-12-03 16:26:46 -0800

    Update gameplay UI

---
+++ BCIJam_2022/Assets/StatusBarUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusBarUI : MonoBehaviour {
+
+	[Range(0, 1)] public float fillAmount = 1.0f;
+
+	public void Update() {
+
+	}
+}
